36e96fa28538d6d2808f3f93059dcbe2ab92b8f5
Michael Wilke <mwilke16@moundsparkacademy.org>
2013-11-12 13:41:21 -0600

    Support.php (No styles

--- support.php
+++ support.php
@@ -6,19 +6,9 @@
 <br />
 <br />
     <h1>Tech Support</h1>
-        <p>Please please Try these first. If the things below do not work feel free to submit a Support Ticket and we shall get back to you</p>
-    <table border="2">
-        <tr>
-            <td><b>Problem</b></td>
-            <td><b>Solution</b></td>
-        </tr>
-        <tr>
-            <td>Audio isn't working or is blocked</td>
-            <td>Try Restarting your Laptop or Computer</td>
-        </tr>
-    </table>
+        <p>Please try restarting your laptop <b><i>BEFORE</i></b> you submit a support ticket. If a computer Restart does not fix it, then please by all means submit a ticket.</p>
 
-    <form action="MAILTO:someone@example.com" method="post" enctype="text/plain">
+    <form action="MAILTO:mwilke16@moundsparkacademy.org" method="post" enctype="text/plain">
         Name:<br>
             <input type="text" name="name" value="your name"><br>
         E-mail:<br>
@@ -27,8 +17,8 @@
                     <select name="mydropdown">
                         <option value="default">----- Choose Your OS -----</option>
                         <option value="xp">Windows XP/Vista</option>
-                        <option value="Milk">Windows 7</option>
-                        <option value="Cheese">Windows 8</option>
+                        <option value="W7">Windows 7</option>
+                        <option value="W8">Windows 8</option>
                         <option value="Mac">--------- Mac OS ---------</option>
                         <option value="10.5/6">Mac OS X 10.5/6 Snow/Leopard</option>
                         <option value="10.7">Mac OS X 10.7 Lion</option>
@@ -36,7 +26,7 @@
                         <option value="10.9">Mac OS X 10.9 Mavericks</option>
                     </select>
                 </div>
-        Comment:<br>
+        Please explain your problem bellow. Please give as much detail as possible:<br>
             <input type="text" name="comment" value="your comment" size="50"><br><br>
             <input type="submit" value="Send">
             <input type="reset" value="Reset">
