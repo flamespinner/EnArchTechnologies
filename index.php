0db40f28f053ccc0e7a0ebf0c7b4e48111d9b603
Riley Lannon <rlannon16@moundsparkacademy.org>
2014-02-09 21:58:47 -0600

    Main Page Content
    Changed to Grayscale (Bootstrap)

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Riley Lannon">
+
+    <title>EnArch | Home</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Fonts -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom Theme CSS -->
+    <link href="css/grayscale.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#page-top">
+                    <i class="fa fa-play-circle"></i>  <span class="light">EnArch</span> Home
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#about">About</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#design">Web Design</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#support">Tech Support</a>
+                    </li>
+                    
+                    <li class="page-scroll">
+                        <a href="#dev">Software</a>
+                    </li>
+                    
+                    <li class="page-scroll">
+                        <a href="#contact">Contact</a>
+                    </li>
+                    
+                    <li>
+                    <a href="Summer@MPA/index.html">Classes</a>
+                        </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <section class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">EnArch Tech</h1>
+                        <p class="intro-text">Technology Made Simple. Technology The Way You Want It.</p>
+                        <div class="page-scroll">
+                            <a href="#about" class="btn btn-circle">
+                                <i class="fa fa-angle-double-down animated"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>About EnArch</h2>
+                <p>EnArch is a small technology company based out of Minnesota, USA, in partnership with <a href="http://www.dbztech.com">DBZ Tech</a>.</p>
+                <p>We do Web Design, Game and Software Development, and have a variety of original products you can purchase -- right here on our website.</p>
+                <p>We only have a handful of "employees", but we have a lot in the works, and are very dedicated to what we do. Our goal is to make technology simple, understandable, and enjoyed by all people.</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="design" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Web Development</h2>
+                    <p>We are excited to make your dream become a reality. With talented web developers experienced in HTML, CSS, PHP, and JavaScript, we are ready to take on whatever challenge you throw at us. We have low rates and are happy to make your site exactly the way you want it.</p>
+                    <a href="http://startbootstrap.com/grayscale" class="btn btn-default btn-lg">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="support" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Tech Support</h2>
+                <p>Have a computer problem? Don't know what to do? It's okay -- you've got us!</p>
+                <p>If you are having a technical issue, a restart may fix whatever problem you're having (Internet issue? Try turning off your wifi, or router, or browser, or all of them. Computer issue? Try to restart it).</p>
+                <p>If a restart doesn't fix it, visit our FAQ. If your problem isn't listed there, we'll be happy to take a look.</p>
+            </div>
+        </div>
+    </section>
+    
+    <section id="dev" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Game and Software Development</h2>
+                <p>Here at EnArch, we take enjoyment in designing games and software. We have people ready to design using C++, TES/CS, and CryEngine.</p>
+                <p>We are also currently designing a computer operating system, a simple game console, writing tutorials, and various other games and products. More information will be posted on the website later.</p>
+            </div>
+        </div>
+    </section>
+    
+    <section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contact Us</h2>
+                <p>Feel free to contact us for feedback, questions, or business inquiries!</p>
+                <p>enarch@gmail.com</p>
+                <ul class="list-inline banner-social-buttons">
+                    <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                    <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <div id="map"></div>
+
+    <!-- Core JavaScript Files NEEDS TO STAY - FANCY SLIDE EFFECTS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/grayscale.js"></script>
+
+</body>
+
+</html>
